029980ae78b86281be88ca55964d6d1c97d5ea6d
Andy Pickler <andy.pickler@gmail.com>
2014-02-09 09:24:36 -0700

    Annotate models with schema

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: line_items
+#
+#  id           :integer          not null, primary key
+#  Purchaser_id :integer
+#  Product_id   :integer
+#  Merchant_id  :integer
+#  Order_id     :integer
+#  quantity     :integer
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class LineItem < ActiveRecord::Base
   belongs_to :Purchaser
   belongs_to :Product
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  description :string(255)
+#  price       :decimal(, )
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Product < ActiveRecord::Base
 end
