c8cb46ac6b90b7d9918858c0f258864697c77bb7
Andy Pickler <andy.pickler@gmail.com>
2014-02-09 09:27:00 -0700

    Calculate the total for an 'order'

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,4 +8,13 @@
 #
 
 class Order < ActiveRecord::Base
+  has_many :line_items
+
+  def total
+    total = 0
+    line_items.each do |line_item|
+      total += (line_item.quantity * line_item.product.price)
+    end
+    total
+  end
 end
